a5e8206dcd5ac8726aeadd822de2da5f47def6af
unknown <Alexandre-Junior01>
2025-03-26 09:25:06 -0300

    add de arquivo ex02

---
+++ Exercio02.py
@@ -0,0 +1,10 @@
+'''
+comandos git
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
+git init
+git status
+git add .
+git commit -m " qualquer coisa"
+'''
+print('nome do aluno: alexandre')
