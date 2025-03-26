b5d52957c91a80fecf69caba65cfcfdb082a6810
unknown <Alexandre-Junior01>
2025-03-26 10:34:02 -0300

    add e alteração codigo

--- exercicio01.py
+++ exercicio01.py
@@ -1,11 +1,12 @@
 '''
-comandos git
+      comandos
+      git init
 git config --global user.email "you@example.com"
 git config --global user.name "Your Name"
-git init
+
 git status
 git add .
-git commit -m " qualquer coisa"
+git commit -m " qualquer coisa" =adicionar novos arquivos
 '''
 aluno= input("Digite seu nome: ")
 print(f"Olá {aluno},tenha um bom dia!")
