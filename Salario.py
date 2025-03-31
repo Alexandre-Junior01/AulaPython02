a2ee4d7196c5189011c2c946001997ea013657ac
unknown <Alexandre-Junior01>
2025-03-31 08:34:35 -0300

    novo codigo

--- Salario.py
+++ Salario.py
@@ -1,4 +1,4 @@
 nome = input("DIgite seu nome : ")
-idade= float(input("Digite sua idade: "))
+idade= int(input("Digite sua idade: "))
 salario = float(input("Digite seu salario: "))
 print(f'Olá {nome}, você tem {idade}, e seu salario é {salario}')
